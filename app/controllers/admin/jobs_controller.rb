d0bfc3e6c2009d00b0622d46d6f7abc9901f439c
HIKKA-WJ <email@15026924000@163.com>
2017-01-21 15:53:51 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,15 +1,16 @@
-class JobsController < ApplicationController
+class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
 
   def show
     @job = Job.find(params[:id])
   end
 
   def index
-     @jobs = Job.all
-   end
+    @jobs = Job.all
+  end
 
-   def new
+  def new
     @job = Job.new
   end
 
@@ -17,16 +18,10 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
 
     if @job.save
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
       render :new
     end
-  end
-
-  private
-
-  def job_params
-    params.require(:job).permit(:title, :description)
   end
 
   def edit
@@ -36,7 +31,7 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
       render :edit
     end
@@ -47,7 +42,13 @@ class JobsController < ApplicationController
 
     @job.destroy
 
-    redirect_to jobs_path
+    redirect_to admin_jobs_path
   end
 
-end
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+  
